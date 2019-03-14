bf551fa06461e8c4a6a475b26a44fadbf9defa41
resab <1421028851@qq.com>
2019-03-14 13:42:07 +0800

    1231

---
+++ src/ch02/task1.java
@@ -0,0 +1,25 @@
+package ch02;
+
+import java.util.Scanner;
+
+/**
+ * 作业1
+ * 
+ * @author Administrator
+ *
+ */
+
+public class task1 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		 System.out.println("输入两行字");
+		int a = input.nextInt();
+		double vNum = input.nextDouble();
+		 System.out.println("刚刚输入了"+a+"   "+vNum);
+		 
+
+	}
+
+}
